7f2979167718dd338f60290417271d1a94a4b598
Moshe Kolodny <mkolodny@netflix.com>
2023-05-16 16:10:46 -0400

    ensure build folder exists

--- build.js
+++ build.js
@@ -1,13 +1,19 @@
 const fs = require('fs');
 const { spawnSync } = require('child_process');
 
+try {
+  fs.mkdirSync('build');
+} catch (e) {}
+
 const examples = fs.readdirSync('examples');
 for (const example of examples) {
-  const stat = fs.statSync(`examples/${example}`);
+  const cwd = `examples/${example}`;
+  const stat = fs.statSync(cwd);
   if (stat.isDirectory()) {
     console.log(`running build for examples/${example}`);
+    spawnSync('npm', ['install'], { cwd, stdio: 'inherit' });
     spawnSync('npm', ['run', 'build', '--if-present'], {
-      cwd: `examples/${example}`,
+      cwd,
       stdio: 'inherit',
     });
   }
