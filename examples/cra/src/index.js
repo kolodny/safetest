e6dd12a10993ffa57e736ebedfbb621816e4b947
Moshe Kolodny <mkolodny@netflix.com>
2023-05-17 19:51:47 -0400

    include lazy load

--- examples/cra/src/index.js
+++ examples/cra/src/index.js
@@ -5,13 +5,16 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 // project imports
-import * as serviceWorker from 'serviceWorker';
 import App from 'App';
 import { store } from 'store';
 
 // style + assets
 // import 'assets/scss/style.scss';
 import config from './config';
+
+const foo = (s) => import(`${s.replace(/.*src/, '.').replace(/\.safetest$/, '')}.safetest`);
+
+window.foo = foo;
 
 // ==============================|| REACT DOM RENDER  ||============================== //
 
@@ -28,4 +31,3 @@ root.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
