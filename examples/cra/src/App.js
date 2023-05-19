f236ceb678104e2c2d756bcb423f43c7256d14f1
Moshe Kolodny <mkolodny@netflix.com>
2023-05-19 12:34:43 -0400

    lint working in cra

--- examples/cra/src/App.js
+++ examples/cra/src/App.js
@@ -1,31 +1,15 @@
-import { useSelector } from 'react-redux';
-
-import { ThemeProvider } from '@mui/material/styles';
-import { CssBaseline, StyledEngineProvider } from '@mui/material';
-
 // routing
 import Routes from 'routes';
 
-// defaultTheme
-import themes from 'themes';
-
-// project imports
-import NavigationScroll from 'layout/NavigationScroll';
+import { Provider } from 'Provider';
 
 // ==============================|| APP ||============================== //
 
 const App = () => {
-    const customization = useSelector((state) => state.customization);
-
     return (
-        <StyledEngineProvider injectFirst>
-            <ThemeProvider theme={themes(customization)}>
-                <CssBaseline />
-                <NavigationScroll>
-                    <Routes />
-                </NavigationScroll>
-            </ThemeProvider>
-        </StyledEngineProvider>
+        <Provider>
+            <Routes />
+        </Provider>
     );
 };
 
---
+++ examples/cra/src/Provider.js
@@ -0,0 +1,30 @@
+import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux';
+
+import { ThemeProvider } from '@mui/material/styles';
+import { CssBaseline, StyledEngineProvider } from '@mui/material';
+
+// defaultTheme
+import themes from 'themes';
+
+// project imports
+import NavigationScroll from 'layout/NavigationScroll';
+
+// ==============================|| APP ||============================== //
+
+export const Provider = (props) => {
+    const customization = useSelector((state) => state.customization);
+
+    return (
+        <StyledEngineProvider injectFirst>
+            <ThemeProvider theme={themes(customization)}>
+                <CssBaseline />
+                <NavigationScroll>{props.children}</NavigationScroll>
+            </ThemeProvider>
+        </StyledEngineProvider>
+    );
+};
+
+Provider.propTypes = {
+    children: PropTypes.node
+};
