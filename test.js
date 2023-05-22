2ae6da009b3d64556dc26ad51c5b8677ce7d885f
Moshe Kolodny <mkolodny@netflix.com>
2023-05-22 12:44:01 -0400

    forgot file

---
+++ test.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const { spawnSync } = require('child_process');
+
+try {
+  fs.mkdirSync('build');
+} catch (e) {}
+
+const examples = fs.readdirSync('examples');
+for (const example of examples) {
+  const cwd = `examples/${example}`;
+  const stat = fs.statSync(cwd);
+  if (stat.isDirectory()) {
+    console.log(`running build for examples/${example}`);
+    spawnSync('npm', ['install'], { cwd, stdio: 'inherit' });
+    spawnSync('npm', ['run', 'safetest:ci', '--if-present'], {
+      cwd,
+      stdio: 'inherit',
+    });
+  }
+}
