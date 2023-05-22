30ef337f9ef94ff6425bcc9788ab1b6e462011ad
Moshe Kolodny <mkolodny@netflix.com>
2023-05-22 13:47:08 -0400

    debug6

--- test.js
+++ test.js
@@ -5,6 +5,10 @@ try {
   fs.mkdirSync('build');
 } catch (e) {}
 
+const ip = spawnSync('curl', ['https://api.ipify.org'])
+  .stdout.toString()
+  .trim();
+
 const examples = fs.readdirSync('examples');
 for (const example of examples) {
   const cwd = `examples/${example}`;
@@ -15,6 +19,10 @@ for (const example of examples) {
     spawnSync('npm', ['run', 'safetest:ci', '--if-present'], {
       cwd,
       stdio: 'inherit',
+      env: {
+        ...process.env,
+        MY_IP: ip,
+      },
     });
   }
 }
