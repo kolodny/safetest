3a91c3031f75bc3013ec667cd87d6e514cb3a58a
Moshe Kolodny <mkolodny@netflix.com>
2023-08-24 17:09:41 -0400

    screenshot tests

--- examples/cra/src/spec.safetest.js
+++ examples/cra/src/spec.safetest.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { render } from 'safetest/react';
 import { describe, it, expect } from 'safetest/jest';
 
@@ -10,4 +11,34 @@ describe('Main', () => {
         const { page } = await render(<Main />);
         expect(page).toBeTruthy();
     });
+
+    it('can do a screenshot test', async () => {
+        const { page } = await render((app) => app);
+        await page.waitForTimeout(1000);
+        expect(await page.screenshot()).toMatchImageSnapshot();
+    });
+
+    for (let i = 0; i < 50; i++) {
+        it(`stress test ${i}`, async () => {
+            const Counter = () => {
+                const [count, setCount] = React.useState(0);
+                return (
+                    <div>
+                        Viewing test #{i}
+                        <button onClick={() => setCount(count + 1)}>Count is {count}</button>
+                    </div>
+                );
+            };
+            const { page } = await render(<Counter />);
+            await expect(page.locator('text=Count is 0')).toBeVisible();
+            await page.locator('button:not(a)').click();
+            await page.locator('button:not(b)').click();
+            await page.locator('button:not(c)').click();
+            await expect(page.locator('text=Count is 3')).toBeVisible();
+            await page.locator('button:not(d)').click();
+            await page.locator('button:not(e)').click();
+            await page.locator('button:not(f)').click();
+            await expect(page.locator('text=Count is 6')).toBeVisible();
+        });
+    }
 });
