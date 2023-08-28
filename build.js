9649e51915a6495188efb2d38c08337bc17c4b70
Moshe Kolodny <mkolodny@netflix.com>
2023-08-28 01:03:02 -0400

    collect all pages

--- build.js
+++ build.js
@@ -6,10 +6,12 @@ try {
 } catch (e) {}
 
 const examples = fs.readdirSync('examples');
+const dirs = [];
 for (const example of examples) {
   const cwd = `examples/${example}`;
   const stat = fs.statSync(cwd);
   if (stat.isDirectory()) {
+    dirs.push(example);
     console.log(`running build for examples/${example}`);
     spawnSync('npm', ['install'], { cwd, stdio: 'inherit' });
     spawnSync('npm', ['run', 'build', '--if-present'], {
@@ -18,3 +20,7 @@ for (const example of examples) {
     });
   }
 }
+fs.writeFileSync(
+  'build/index.html',
+  dirs.map((dir) => `<a href="${dir}">${dir}</a>`).join('<br>')
+);
