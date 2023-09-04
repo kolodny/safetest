61f40d7ca67a78cb0bf128a688358f9a40d8d78f
Moshe Kolodny <mkolodny@netflix.com>
2023-09-04 15:53:06 -0400

    add more vue tests

--- examples/vite-vue-ts/src/App.safetest.tsx
+++ examples/vite-vue-ts/src/App.safetest.tsx
@@ -14,6 +14,30 @@ describe('Example', () => {
     await expect(page.locator('text=Count is 2')).toBeVisible();
   });
 
+  it('can do many interactions fast', async () => {
+    const { page } = await render(<Counter startingCount={0} />);
+    await expect(page.locator('text=Count is 0')).toBeVisible();
+    for (let i = 1; i <= 500; i++) {
+      await page.locator('button:not(a)').click();
+      await expect(page.locator(`text=Count is ${i}`)).toBeVisible();
+    }
+  });
+
+  for (let i = 0; i < 50; i++) {
+    it(`stress test ${i} run`, async () => {
+      const { page } = await render(<Counter startingCount={0} />);
+      await expect(page.locator('text=Count is 0')).toBeVisible();
+      await page.locator('button:not(a)').click();
+      await page.locator('button:not(b)').click();
+      await page.locator('button:not(c)').click();
+      await expect(page.locator('text=Count is 3')).toBeVisible();
+      await page.locator('button:not(d)').click();
+      await page.locator('button:not(e)').click();
+      await page.locator('button:not(f)').click();
+      await expect(page.locator('text=Count is 6')).toBeVisible();
+    });
+  }
+
   it('can render the entire page', async () => {
     const { page } = await render();
     await expect(
