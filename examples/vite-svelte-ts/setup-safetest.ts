acd209bad78baa52525921f93c463e375655b91c
Moshe Kolodny <mkolodny@netflix.com>
2023-09-12 23:06:19 -0400

    fix

--- examples/vite-svelte-ts/setup-safetest.ts
+++ examples/vite-svelte-ts/setup-safetest.ts
@@ -3,7 +3,7 @@ import { setup } from 'safetest/setup';
 vitest.setConfig({ testTimeout: 30000 });
 
 setup({
-  bootstrappedAt: require.resolve('./src.main.ts'),
+  bootstrappedAt: require.resolve('./src/main.ts'),
   ciOptions: {
     usingArtifactsDir: '../../build/vite-svelte-ts/artifacts',
   },
