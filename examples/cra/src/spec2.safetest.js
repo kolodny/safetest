22878f3527f0786e97aa7f1ea703abb2d0564698
Moshe Kolodny <mkolodny@netflix.com>
2023-10-09 23:00:30 -0400

    multiple files

---
+++ examples/cra/src/spec2.safetest.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import { render } from 'safetest/react';
+import { describe, it, expect, retryTimes, browserMock } from 'safetest/jest';
+
+retryTimes(3);
+
+export const Main = () => {
+    return <>This is cool</>;
+};
+
+describe('Main', () => {
+    it('should render', async () => {
+        const { page } = await render(<Main />);
+        expect(page).toBeTruthy();
+    });
+
+    it('can do a screenshot test', async () => {
+        const { page } = await render((app) => app);
+        await page.evaluate(() => document.querySelector('[aria-label="Live Customize"]').remove());
+        await page.waitForTimeout(3000);
+        expect(await page.screenshot()).toMatchImageSnapshot({ failureThreshold: 10 });
+    });
+
+    it('can check that a spy was called', async () => {
+        const spy = browserMock.fn();
+        const { page } = await render(<button onClick={() => spy('foo')}>Click me</button>);
+        expect(await spy).not.toHaveBeenCalled();
+        await page.locator('text=Click me').click();
+        expect(await spy).toHaveBeenCalled();
+        expect(await spy).toHaveBeenCalledWith('foo');
+    });
+
+    it('can do many interactions fast', async () => {
+        const Counter = () => {
+            const [count, setCount] = React.useState(0);
+            return (
+                <div>
+                    <button onClick={() => setCount(count + 1)}>Count is {count}</button>
+                </div>
+            );
+        };
+        const { page } = await render(<Counter />);
+        await expect(page.locator('text=Count is 0')).toBeVisible();
+        for (let i = 1; i <= 500; i++) {
+            await page.locator('button:not(a)').click();
+            await expect(page.locator(`text=Count is ${i}`)).toBeVisible();
+        }
+    });
+
+    for (let i = 0; i < 50; i++) {
+        it(`stress test ${i} run`, async () => {
+            const Counter = () => {
+                const [count, setCount] = React.useState(0);
+                return (
+                    <div>
+                        Viewing test #{i}
+                        <button onClick={() => setCount(count + 1)}>Count is {count}</button>
+                    </div>
+                );
+            };
+            const { page } = await render(<Counter />);
+            await expect(page.locator('text=Count is 0')).toBeVisible();
+            await page.locator('button:not(a)').click();
+            await page.locator('button:not(b)').click();
+            await page.locator('button:not(c)').click();
+            await expect(page.locator('text=Count is 3')).toBeVisible();
+            await page.locator('button:not(d)').click();
+            await page.locator('button:not(e)').click();
+            await page.locator('button:not(f)').click();
+            await expect(page.locator('text=Count is 6')).toBeVisible();
+        });
+    }
+});
---
+++ examples/vite-react-ts/src/Another.safetest.tsx
@@ -0,0 +1,103 @@
+import React from 'react';
+import { render } from 'safetest/react';
+import { describe, it, expect } from 'safetest/vitest';
+
+describe.skip('Main2', () => {
+  it('hey loads a simple div', async () => {
+    const { page } = await render(() => <>Testing123</>);
+    expect(await page.screenshot()).toMatchImageSnapshot({});
+  });
+
+  it('Has a landing page', async () => {
+    const { page } = await render();
+    expect(page).toBeTruthy();
+  });
+
+  it('Has a landing page', async () => {
+    const { page } = await render();
+    expect(page).toBeTruthy();
+  });
+
+  it('Does not break regular expects', async () => {
+    const { page } = await render();
+    expect(page).toBeTruthy();
+    expect(1).toEqual(1);
+    expect(1).toBe(1);
+  });
+
+  it('can do many interactions fast', async () => {
+    const Counter = () => {
+      const [count, setCount] = React.useState(0);
+      return (
+        <div>
+          <button onClick={() => setCount(count + 1)}>Count is {count}</button>
+        </div>
+      );
+    };
+    const { page } = await render(<Counter />);
+    await expect(page.locator('text=Count is 0')).toBeVisible();
+    for (let i = 1; i <= 500; i++) {
+      await page.locator('button:not(a)').click();
+      await expect(page.locator(`text=Count is ${i}`)).toBeVisible();
+    }
+  });
+
+  for (let i = 0; i < 50; i++) {
+    it(`stress test ${i} run`, async () => {
+      const Counter = () => {
+        const [count, setCount] = React.useState(0);
+        return (
+          <div>
+            Viewing test #{i}
+            <button onClick={() => setCount(count + 1)}>
+              Count is {count}
+            </button>
+          </div>
+        );
+      };
+      const { page } = await render(<Counter />);
+      await expect(page.locator('text=Count is 0')).toBeVisible();
+      await page.locator('button:not(a)').click();
+      await page.locator('button:not(b)').click();
+      await page.locator('button:not(c)').click();
+      await expect(page.locator('text=Count is 3')).toBeVisible();
+      await page.locator('button:not(d)').click();
+      await page.locator('button:not(e)').click();
+      await page.locator('button:not(f)').click();
+      await expect(page.locator('text=Count is 6')).toBeVisible();
+    });
+  }
+
+  it('can use the bridge function', async () => {
+    let count = 0;
+    let forceNumber: (num: number) => void = () => {};
+    const Counter = () => {
+      const forceRender = React.useReducer(() => count, 0)[1];
+      forceNumber = (n) => {
+        count = n;
+        forceRender();
+      };
+      return (
+        <div>
+          <button
+            onClick={() => {
+              count++;
+              forceRender();
+            }}
+          >
+            Count is {count}
+          </button>
+        </div>
+      );
+    };
+
+    const { page, bridge } = await render(<Counter />);
+    await expect(page.locator('text=Count is 0')).toBeVisible();
+    await page.click('button');
+    await expect(page.locator('text=Count is 1')).toBeVisible();
+    await bridge(() => forceNumber(50));
+    await expect(page.locator('text=Count is 50')).toBeVisible();
+    await page.click('button');
+    await expect(page.locator('text=Count is 51')).toBeVisible();
+  });
+});
