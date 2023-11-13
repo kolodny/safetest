fe3c8dfdec502e7988ce6cbf8494479c8d7396f3
Moshe Kolodny <mkolodny@netflix.com>
2023-11-13 16:48:09 -0500

    add more artifacts

--- build.js
+++ build.js
@@ -25,7 +25,7 @@ fs.writeFileSync(
     dirs
       .map(
         (dir) =>
-          `<td><a href="${dir}">${dir} App</a></td><td><a href="${dir}/artifacts/results.html">${dir} Artifacts</a>`
+          `<td><a href="${dir}">${dir} App</a></td><td><a href="https://safetest-two.vercel.app/report.html#results=${dir}/artifacts/results.json&base=/${dir}/">${dir} Artifacts</a>`
       )
       .join('</tr></tr>')
 );
