16598f818e0a8b4ba629522b2147dc2162774589
Moshe Kolodny <mkolodny@netflix.com>
2024-02-22 12:03:45 -0500

    use vanilla jest

--- examples/angular-app/jest.config.js
+++ examples/angular-app/jest.config.js
@@ -1,5 +1,16 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
+  testMatch: ["<rootDir>/**/*.safetest.ts"],
+  setupFilesAfterEnv: ["./setup-safetest.js"],
+  testTimeout: 30000,
+  transform: {
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      {
+        // ts-jest configuration goes here
+      },
+    ],
+  },
 };
