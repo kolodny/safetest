e940f5d629091aa86e7ee8f0854176d4749a1aeb
Moshe Kolodny <mkolodny@netflix.com>
2024-02-22 11:33:52 -0500

    remove command line env overrides

--- examples/angular-app/setup-safetest.js
+++ examples/angular-app/setup-safetest.js
@@ -2,6 +2,8 @@ const { setup } = require("safetest/setup");
 
 setup({
   bootstrappedAt: require.resolve("./src/main.ts"),
+  url: "http://localhost:4200/angular-app",
+  headless: false,
   ciOptions: {
     usingArtifactsDir: "artifacts",
   },
